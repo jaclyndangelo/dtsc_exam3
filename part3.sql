c6a36b5f849984742cc9490420bc35ce4676641e
Jaclyn D'Angelo <54593484+jaclyndangelo@users.noreply.github.com>
2019-12-10 13:13:46 -0500

    Update part3.sql

--- part3.sql
+++ part3.sql
@@ -2,17 +2,27 @@
 SELECT *
 FROM customer;
 
-SELECT LastName, firstname, address
+SELECT LastName, FirstName, Address
 FROM Employee;
 
 SELECT Track.Name, Album.title
 FROM Track
 LEFT JOIN Album
-on Track.AlbumId = Album.AlbumId;
+ON Track.AlbumId = Album.AlbumId;
 
-SELECT t.Name, a.Title, ar.Name
+SELECT t.Name, a.Title, g.Name
 FROM Track as t
 LEFT JOIN Album as a
-on t.AlbumId = a.AlbumId
-LEFT JOIN Artist
-where GenreName = 'Punk';
+ON t.AlbumId = a.AlbumId
+LEFT JOIN genre as g
+ON t.genreId = g.genreId
+WHERE Genre.Name = 'Punk';
+
+
+SELECT t.composer, a.title, r.Name
+FROM Track as t
+JOIN Artist as a
+ON t.albumid = a.albumid
+JOIN artist as r
+ON r.artistId = a.artistId
+WHERE r.name = 'Insane Clown Posse';
